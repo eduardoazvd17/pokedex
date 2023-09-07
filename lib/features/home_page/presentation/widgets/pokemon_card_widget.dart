36605f4fb16a718b3fe01b96e29ba61c704e0656
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-09-07 15:39:14 -0300

    Fixing card widget overlay

--- lib/features/home_page/presentation/widgets/pokemon_card_widget.dart
+++ lib/features/home_page/presentation/widgets/pokemon_card_widget.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:pokedex/features/home_page/data/enums/pokemon_type.dart';
 import 'package:pokedex/features/home_page/presentation/widgets/pokemon_type_tag_widget.dart';
 
+import '../../../../core/data/utils/app_icons.dart';
 import '../../../../core/data/utils/app_theme.dart';
 import '../../data/models/pokemon_model.dart';
 
@@ -17,13 +19,12 @@ class PokemonCardWidget extends StatelessWidget {
         children: [
           Card(
             elevation: 1,
-            color: Colors.blue[300],
+            color: pokemonModel.types.first.backgroundColor,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(25),
             ),
             child: _content,
           ),
-          Positioned(right: 0, top: 0, bottom: 0, child: _pokeballOverlayImage),
           Positioned(top: 10, right: 10, child: _favoriteButton),
         ],
       ),
@@ -31,32 +32,37 @@ class PokemonCardWidget extends StatelessWidget {
   }
 
   Widget get _content {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
+    return Stack(
       children: [
-        Flexible(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 16, bottom: 16, top: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(pokemonModel.formattedOrder),
-                Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Text(
-                    pokemonModel.name,
-                    style: AppTheme.title25w600,
-                  ),
+        Positioned(right: 0, top: 0, bottom: 0, child: _pokeballOverlayImage),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16, bottom: 16, top: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(pokemonModel.formattedOrder),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Text(
+                        pokemonModel.name,
+                        style: AppTheme.title25w600,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: _pokemonTypeTagsWidgets,
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10.0),
-                  child: _pokemonTypeTagsWidgets,
-                ),
-              ],
+              ),
             ),
-          ),
+            _pokemonImageWidget,
+          ],
         ),
-        _pokemonImageWidget,
       ],
     );
   }
@@ -80,12 +86,13 @@ class PokemonCardWidget extends StatelessWidget {
 
   Widget get _pokeballOverlayImage {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Image.asset(
-        'assets/images/pokeball-overlay.png',
-        color: Colors.black12,
-        filterQuality: FilterQuality.low,
-        opacity: const AlwaysStoppedAnimation(0.3),
+      padding: const EdgeInsets.only(right: 10.0),
+      child: SvgPicture.asset(
+        'assets/images/pokeball-overlay.svg',
+        colorFilter: const ColorFilter.mode(
+          Colors.black38,
+          BlendMode.srcIn,
+        ),
       ),
     );
   }
@@ -93,11 +100,7 @@ class PokemonCardWidget extends StatelessWidget {
   Widget get _favoriteButton {
     return IconButton(
       onPressed: () {},
-      icon: const Icon(
-        CupertinoIcons.heart,
-        color: Colors.black,
-        size: 40,
-      ),
+      icon: AppIcons.heart(isSelected: false),
     );
   }
 }
