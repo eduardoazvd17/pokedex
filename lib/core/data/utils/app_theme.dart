3940c76c6d8bb5986f9b6e030f8ee82aa871f8d5
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-09-07 13:28:00 -0300

    Creating app theme class

---
+++ lib/core/data/utils/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static Color get primaryColor => Colors.red;
+  static Color get backgroundColor => Colors.white;
+
+  static TextStyle get title25w600 => const TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.w600,
+      );
+
+  static ThemeData get lightTheme => ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: primaryColor,
+          surfaceTint: backgroundColor,
+        ),
+        fontFamily: GoogleFonts.roboto().fontFamily,
+        useMaterial3: true,
+      );
+}
